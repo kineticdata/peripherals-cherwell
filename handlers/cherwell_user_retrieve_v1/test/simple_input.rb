175964b292764625d945641df23e046530b2e70e
mhowe <matthew.howe@kineticdata.com>
2019-10-27 12:40:20 -0400

    Added User Lookup v1 Handler
    This hander uses the V1 Cherwell API.

---
+++ handlers/cherwell_user_retrieve_v1/test/simple_input.rb
@@ -0,0 +1,13 @@
+{
+  'info' => {
+    'api_location' => '',
+    'client_id' => '',
+    'client_secret' => '',
+    'username' => '',
+    'password' => '',
+    'enable_debug_logging'=>'yes'
+  },
+  'parameters' => {
+    'login_id' => ''
+  }
+}
